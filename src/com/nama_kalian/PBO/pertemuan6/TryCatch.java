9332502b5a9f6fbb5478114fc2779f73a789d400
Rizfan <rispanherlaya@gmail.com>
2023-05-24 13:37:46 +0700

    Pertemuan 6

---
+++ src/com/nama_kalian/PBO/pertemuan6/TryCatch.java
@@ -0,0 +1,18 @@
+package com.nama_kalian.PBO.pertemuan6;
+
+public class TryCatch {
+    public static void main(String[] args) {
+        // Membuat Array
+        int[] nomor = {1, 2, 3, 4, 5};
+
+        try {
+            System.out.println(nomor[4]);
+        }
+
+        catch(ArrayIndexOutOfBoundsException e) {
+            System.out.println("Array nya melebihi batas!");
+        }
+
+        System.out.println("Proses Selesai!");
+    }
+}
